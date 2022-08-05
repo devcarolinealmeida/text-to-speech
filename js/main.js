761af21276c41327610f28e75f70cc06b85fdd85
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-05 12:12:47 +0200

    correcting bug on safari

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 const chooseLanguage = document.querySelector("select")
 const enterText = document.querySelector("textarea")
 const convertSpeech = document.getElementById("convert-speech")
+const synth = window.speechSynthesis;
 
-let voices = []
+let voices
 
-speechSynthesis.addEventListener("voiceschanged", () => {
-    voices = speechSynthesis.getVoices()
-
+function loadVoices() {
+    voices = synth.getVoices();
     voices.forEach(({ name, lang }) => {
         const option = document.createElement("option")
 
         option.value = name
-        option.textContent = `${lang} | ${name}`
+        option.textContent = `${name} | ${lang}`
         chooseLanguage.appendChild(option)
     })
-})
+}
+  
+  // to load in Safari
+if ('onvoiceschanged' in synth) {
+    synth.onvoiceschanged = loadVoices;
+  } else {
+    loadVoices();
+}
+
 
 const speech = new SpeechSynthesisUtterance()
 const setText = text => {
@@ -37,3 +45,49 @@ const selectVoice = event => {
 }
 
 chooseLanguage.addEventListener("change", selectVoice)
+
+
+
+//-------------
+
+
+// //resolucao Roger Melo (nao carrega select no safari)
+// const chooseLanguage = document.querySelector("select")
+// const enterText = document.querySelector("textarea")
+// const convertSpeech = document.getElementById("convert-speech")
+
+// let voices = []
+
+// speechSynthesis.addEventListener("voiceschanged", () => {
+//     voices = speechSynthesis.getVoices()
+
+//     voices.forEach(({ name, lang }) => {
+//         const option = document.createElement("option")
+
+//         option.value = name
+//         option.textContent = `${lang} | ${name}`
+//         chooseLanguage.appendChild(option)
+//     })
+// })
+
+// const speech = new SpeechSynthesisUtterance()
+// const setText = text => {
+//     speech.text = enterText.value
+// }
+
+// const speakText = () => {
+//     speechSynthesis.speak(speech)
+// }
+
+// convertSpeech.addEventListener("click", () => {
+//     setText(enterText.value)
+//     speakText()
+// })
+
+// const selectVoice = event => {
+//     const choseVoice = voices.find(voice => voice.name === event.target.value)
+//     speech.voice = choseVoice
+//     console.log(choseVoice)
+// }
+
+// chooseLanguage.addEventListener("change", selectVoice)
