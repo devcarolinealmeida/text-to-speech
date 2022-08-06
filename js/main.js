e53e35c62379358fe2ba24d43583fcedc33301f2
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-06 10:33:42 +0200

    add div expression-box

--- js/main.js
+++ js/main.js
@@ -46,13 +46,39 @@ const selectVoice = event => {
 
 chooseLanguage.addEventListener("change", selectVoice)
 
-//------------
+//---------------------------------------------------
 // add box-images
 
+const main = document.querySelector("main")
+
+const expressions = [
+    { img: "../img/angry.jpg", text: "I'm angry" },
+    { img: "../img/drink.jpg", text: "I'm thirsty" },
+    { img: "../img/food.jpg", text: "I'm hungry" },
+    { img: "../img/happy.jpg", text: "I'm happy" },
+    { img: "../img/home.jpg", text: "I want to go home" },
+    { img: "../img/hurt.jpg", text: "I'm hurt" },
+    { img: "../img/outside.jpg", text: "I want to go outside" },
+    { img: "../img/sad.jpg", text: "I'm sad" },
+    { img: "../img/scared.jpg", text: "I'm scared" },
+    { img: "../img/tired.jpg", text: "I'm tired" },
+    { img: "../img/school.jpg", text: "I want to go to school" },
+    { img: "../img/grandma.jpg", text: "I want to see grandma"}
+]
+
+function createExpressionsBox(expressions) {
+    const div = document.createElement("div")
+    div.classList.add("expression-box")
+    div.innerHTML = `<img src="${expressions.img}" alt="${expressions.text}" 
+    <p>${expressions.text}</p`
+    main.appendChild(div)
+}
+
+expressions.forEach(createExpressionsBox)
 
 
-//-------------
 
+//---------------------------------------------------
 
 // DOES NOT WORK IN safari)
 // const chooseLanguage = document.querySelector("select")
